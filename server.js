5c40871e53ef94daa4ac7a2253955598f656b30e
xXYvesXx <yvesh20@gmail.com>
2020-11-05 18:46:49 -0300

    configurações iniciais das rotas

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
-
 app.set("port", port);
+
+const server = http.createServer(app);
+const router = express.Router();
 
 app.listen(port, () => {
   console.log("Server is on port " + port);
 });
+
+var route = router.get("/", (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1",
+  });
+});
+
+app.use("/", route);
