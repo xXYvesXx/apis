8342f570b850e411dbf511e6c544616a0e26faec
xXYvesXx <yvesh20@gmail.com>
2020-11-06 18:03:59 -0300

    rota com o DELETE

--- src/app.js
+++ src/app.js
@@ -22,11 +22,16 @@ const create = router.post("/", (req, res, next) => {
 
 const put = router.put("/:id", (req, res, next) => {
   const id = req.params.id;
-  res.status(201).send({ id: id, item: req.body });
+  res.status(200).send({ id: id, item: req.body });
+});
+
+const del = router.delete("/", (req, res, next) => {
+  res.status(200).send(req.body);
 });
 
 app.use("/", route);
 app.use("/products", create);
 app.use("/products", put);
+app.use("/products", del);
 
 module.exports = app;
