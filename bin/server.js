6fec72f663664b91bd6c6fbf8e44c782fd60e078
xXYvesXx <yvesh20@gmail.com>
2020-11-05 21:22:19 -0300

    modularização de código

--- bin/server.js
+++ bin/server.js
@@ -4,9 +4,7 @@ const http = require('http')
 const debug = require('debug')('nodestr:server')
 const express = require('express')
 
-const app = express()
 const port = normalizePort(process.env.PORT || '3000')
-app.set('port', port)
 
 const server = http.createServer(app)
 const router = express.Router()
@@ -17,8 +15,6 @@ const route = router.get('/', (req, res, next) => {
     version: '0.0.1',
   })
 })
-
-app.use('/', route)
 
 server.listen(port, () => {
   console.log('API rodando na porta ' + port)
