e33b9952bb40a203e1b7c6be6b5292505fc07cae
xXYvesXx <yvesh20@gmail.com>
2020-11-05 21:09:19 -0300

    gerenciador de erros onError

--- server.js
+++ server.js
@@ -1,41 +1,67 @@
-"use strict" /*"Força o Javascript a ser mais criterioso"*/;
+'use strict' /*"Força o Javascript a ser mais criterioso"*/
 
-const http = require("http");
-const debug = require("debug")("nodestr:server");
-const express = require("express");
+const http = require('http')
+const debug = require('debug')('nodestr:server')
+const express = require('express')
 
-const app = express();
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const app = express()
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 
-const server = http.createServer(app);
-const router = express.Router();
+const server = http.createServer(app)
+const router = express.Router()
 
-const route = router.get("/", (req, res, next) => {
+const route = router.get('/', (req, res, next) => {
   res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1",
-  });
-});
+    title: 'Node Store API',
+    version: '0.0.1',
+  })
+})
 
-app.use("/", route);
+app.use('/', route)
 
 server.listen(port, () => {
-  console.log("API rodando na porta " + port);
-});
+  console.log('API rodando na porta ' + port)
+})
+
+server.on('error', onError)
 
 //Normalizando a porta
 
 function normalizePort(val) {
-  const port = parseInt(val, 10);
+  const port = parseInt(val, 10)
 
   if (isNaN(port)) {
-    return val;
+    return val
   }
 
   if (port >= 0) {
-    return port;
+    return port
   }
 
-  return false;
+  return false
+}
+
+//Gerenciador de erros
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated priveleges ')
+      process.exit(1)
+      break
+
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+
+    default:
+      throw error
+  }
 }
